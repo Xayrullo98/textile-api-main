38003bb26c327eb5cb34f445efa89d743ed461a2
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2023-09-07 16:21:01 +0500

    order_done_products_for_mobile

--- functions/order_done_products.py
+++ functions/order_done_products.py
@@ -12,21 +12,26 @@ from utils.db_operations import save_in_db, the_one
 from utils.pagination import pagination
 
 
-def all_order_done_products(order_id, stage_id, from_date, to_date, page, limit, db):
+def all_order_done_products(order_id, stage_id,worker_id, from_date, to_date, page, limit, db):
     order_done_products = db.query(Order_done_products).options(
         joinedload(Order_done_products.order), joinedload(Order_done_products.stage),
         joinedload(Order_done_products.user))
     order_done_product_stats = db.query(Order_done_products, func.sum(Order_done_products.quantity *
-            Order_done_products.kpi_money).label("total_price")).options(joinedload(Order_done_products.stage))
+                                                                      Order_done_products.kpi_money).label(
+        "total_price")).options(joinedload(Order_done_products.stage))
 
     if order_id:
         order_done_products = order_done_products.filter(Order_done_products.order_id == order_id)
         order_done_product_stats = order_done_product_stats.filter(Order_done_products.order_id == order_id)
+    if worker_id:
+        order_done_products = order_done_products.filter(Order_done_products.worker_id == worker_id)
+        order_done_product_stats = order_done_product_stats.filter(Order_done_products.worker_id == worker_id)
     if stage_id:
         order_done_products = order_done_products.filter(Order_done_products.stage_id == stage_id)
         order_done_product_stats = order_done_product_stats.filter(Order_done_products.stage_id == stage_id)
     if from_date and to_date:
-        order_done_products = order_done_products.filter(func.date(Order_done_products.datetime).between(from_date, to_date))
+        order_done_products = order_done_products.filter(
+            func.date(Order_done_products.datetime).between(from_date, to_date))
         order_done_product_stats = order_done_product_stats.filter(func.date(Order_done_products.datetime).
                                                                    between(from_date, to_date))
 
@@ -49,9 +54,13 @@ def one_order_done_product(ident, db):
 
 def create_order_done_product(form, thisuser, db):
     stage = one_stage(id=form.stage_id, db=db)
-    done_product = db.query(Order_done_products).filter(Order_done_products.order_id==form.order_id,
-                                                        Order_done_products.stage_id==form.stage_id,
-                                                        Order_done_products.datetime==datetime.datetime.now().date(),).first()
+    done_product_check = db.query(Order_done_products).filter(Order_done_products.order_id == form.order_id,
+                                                              Order_done_products.stage_id == form.stage_id, ).first()
+    if done_product_check :
+       raise HTTPException(status_code=400,detail="Bu bosqich bajarilgan")
+    done_product = db.query(Order_done_products).filter(Order_done_products.order_id == form.order_id,
+                                                        Order_done_products.stage_id == form.stage_id,
+                                                        Order_done_products.datetime == datetime.datetime.now().date(), ).first()
     if not done_product:
 
         new_order_h_db = Order_done_products(
@@ -66,7 +75,43 @@ def create_order_done_product(form, thisuser, db):
         )
         save_in_db(db, new_order_h_db)
     else:
-        quantity = done_product.quantity+form.quantity
+        quantity = done_product.quantity + form.quantity
+        db.query(Order_done_products).filter(Order_done_products.order_id == form.order_id,
+                                             Order_done_products.stage_id == form.stage_id,
+                                             Order_done_products.datetime == datetime.datetime.now().date(), ).update({
+            Order_done_products.datetime: datetime.datetime.now().date(),
+            Order_done_products.quantity: quantity,
+
+        })
+        db.commit()
+
+    money = form.quantity * stage.kpi
+    add_user_balance(user_id=form.worker_id, money=money, db=db)
+
+def create_order_done_product_for_mobile(form,  db):
+    stage = one_stage(id=form.stage_id, db=db)
+    done_product_check = db.query(Order_done_products).filter(Order_done_products.order_id == form.order_id,
+                                                              Order_done_products.stage_id == form.stage_id, ).first()
+    if done_product_check :
+       raise HTTPException(status_code=400,detail="Bu bosqich bajarilgan")
+    done_product = db.query(Order_done_products).filter(Order_done_products.order_id == form.order_id,
+                                                        Order_done_products.stage_id == form.stage_id,
+                                                        Order_done_products.datetime == datetime.datetime.now().date(), ).first()
+    if not done_product:
+
+        new_order_h_db = Order_done_products(
+            order_id=form.order_id,
+            datetime=datetime.datetime.now().date(),
+            stage_id=form.stage_id,
+            worker_id=form.worker_id,
+            quantity=form.quantity,
+            kpi_money=stage.kpi,
+            user_id=form.worker_id,
+
+        )
+        save_in_db(db, new_order_h_db)
+    else:
+        quantity = done_product.quantity + form.quantity
         db.query(Order_done_products).filter(Order_done_products.order_id == form.order_id,
                                              Order_done_products.stage_id == form.stage_id,
                                              Order_done_products.datetime == datetime.datetime.now().date(), ).update({
--- routes/order_done_products.py
+++ routes/order_done_products.py
@@ -4,7 +4,7 @@ from datetime import date
 from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.orm import Session
 from functions.order_done_products import create_order_done_product, update_order_done_product,\
-    all_order_done_products, one_order_done_product
+    all_order_done_products, one_order_done_product,create_order_done_product_for_mobile
 from routes.login import get_current_active_user
 from utils.role_verification import role_verification
 from schemes.order_done_products import CreateOrder_done_products, UpdateOrder_done_products
@@ -27,7 +27,7 @@ def add_stage_user(form: CreateOrder_done_products, db: Session = Depends(databa
 
 
 @order_done_products_router.get('/', status_code=200)
-def get_order_done_products(stage_id: int = 0, order_id: int = 0,  id: int = 0, page: int = 1,
+def get_order_done_products(stage_id: int = 0, order_id: int = 0,worker_id: int = 0,  id: int = 0, page: int = 1,
                     from_date: date = Query(None), to_date: date = Query(date.today()),
                     limit: int = 25, db: Session = Depends(database),
                     current_user: UserCurrent = Depends(get_current_active_user)):
@@ -35,7 +35,7 @@ def get_order_done_products(stage_id: int = 0, order_id: int = 0,  id: int = 0, 
     if id:
         return one_order_done_product(id, db)
     else:
-        return all_order_done_products(stage_id=stage_id, order_id=order_id, page=page, limit=limit, db=db,from_date=from_date,to_date=to_date)
+        return all_order_done_products(stage_id=stage_id, order_id=order_id,worker_id=worker_id, page=page, limit=limit, db=db,from_date=from_date,to_date=to_date)
 
 
 @order_done_products_router.put("/update")
@@ -47,3 +47,18 @@ def stage_user_update(form: UpdateOrder_done_products, db: Session = Depends(dat
     raise HTTPException(status_code=200, detail="Amaliyot muvaffaqiyatli amalga oshirildi")
 
 
+@order_done_products_router.post('/add_products', )
+def add__order_done_product_for_mobile(form: CreateOrder_done_products, db: Session = Depends(database),       ):
+    create_order_done_product_for_mobile(form=form,  db=db)
+    raise HTTPException(status_code=200, detail="Amaliyot muvaffaqiyatli amalga oshirildi")
+
+
+@order_done_products_router.get('/all', status_code=200)
+def get_order_done_products_for_mobile(stage_id: int = 0, order_id: int = 0,worker_id: int = 0,  id: int = 0, page: int = 1,
+                    from_date: date = Query(None), to_date: date = Query(date.today()),
+                    limit: int = 25, db: Session = Depends(database),
+                                       ):
+    if id:
+        return one_order_done_product(id, db)
+    else:
+        return all_order_done_products(stage_id=stage_id, order_id=order_id,worker_id=worker_id, page=page, limit=limit, db=db,from_date=from_date,to_date=to_date)
