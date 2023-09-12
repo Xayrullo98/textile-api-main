5422836eb485192b8ceb3184da92c4521ad54672
Abdullajon <110884479+AbdullajonOdilov@users.noreply.github.com>
2023-09-12 09:20:23 +0500

    supplier_balances.py changed

--- functions/supplier_balances.py
+++ functions/supplier_balances.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 from utils.db_operations import save_in_db
 from models.supplier_balances import Supplier_balance
 
@@ -14,11 +16,16 @@ def create_supplier_balance_func(balance, currencies_id, supplier_id, db):
 def expense_supplier_balance(money, currency_id, supplier_id, db):
     """Xarajat qilinganda supplier_balansidan ayiramiz"""
 
-    db.query(Supplier_balance).filter(Supplier_balance.supplier_id == supplier_id,
-                                      Supplier_balance.currencies_id == currency_id).update({
-        Supplier_balance.balance: Supplier_balance.balance - money
-    })
-    db.commit()
+    if db.query(Supplier_balance).filter(Supplier_balance.supplier_id == supplier_id,
+                                      Supplier_balance.currencies_id == currency_id).first():
+
+        db.query(Supplier_balance).filter(Supplier_balance.supplier_id == supplier_id,
+                                          Supplier_balance.currencies_id == currency_id).update({
+            Supplier_balance.balance: Supplier_balance.balance - money
+        })
+        db.commit()
+    else:
+        raise HTTPException(status_code=400, detail="Currency_id ni no'tog'ri kiritdingiz")
 
 
 def update_supplier_balance(money, currency_id, supplier_id, db):
